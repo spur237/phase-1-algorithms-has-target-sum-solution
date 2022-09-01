d04583f412520f905044a24d289afd0ca3582da2
spur237 <samuel.nzioka@student.moringaschool.com>
2022-09-01 16:44:20 +0300

    done with assignment

--- index.js
+++ index.js
@@ -1,34 +1,30 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+ for(let i = 0; i < array.length; i++){
+  const complement = target - array[i];
+  for(let j=i+1;j< array.length; j++ ){
+    if(array[j] === complement)
+    return true;
+  }
+ }
+ return false
 }
 
 /* 
   Write the Big O time complexity of your function here
 */
 
+// 0(n)
 /* 
   Add your pseudocode here
 */
 
+// iterate over array
+// identify number that adds to our target
+// iterate over remaining array
+// if any number gives desired XPathResult, reurn true
+// if there is no desired value at the end of the array return false
 /*
   Add written explanation of your solution here
 */
 
-// You can run `node index.js` to view these console logs
-if (require.main === module) {
-  // add your own custom tests in here
-  console.log("Expecting: true");
-  console.log("=>", hasTargetSum([3, 8, 12, 4, 11, 7], 10));
-
-  console.log("");
-
-  console.log("Expecting: true");
-  console.log("=>", hasTargetSum([22, 19, 4, 6, 30], 25));
-
-  console.log("");
-
-  console.log("Expecting: false");
-  console.log("=>", hasTargetSum([1, 2, 5], 4));
-}
-
-module.exports = hasTargetSum;
